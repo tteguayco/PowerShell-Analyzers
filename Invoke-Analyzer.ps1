be6d2b520ba1de16863f1232094b607f7bc6d790
Dávid El-Saig <david.el-saig@lombiq.com>
2022-07-04 16:41:03 +0200

    Make `Invoke-Analyzer` output in MSBuild-compatible format.

--- Invoke-Analyzer.ps1
+++ Invoke-Analyzer.ps1
@@ -1,24 +1,40 @@
 [Diagnostics.CodeAnalysis.SuppressMessageAttribute(
     "PSReviewUnusedParameter",
-    "ForGitHubActions",
+    "ForGitHubActions;ForMsBuild",
     Justification = "False positive (see https://github.com/PowerShell/PSScriptAnalyzer/issues/1472).")]
 param(
     $SettingsPath = (Join-Path (Split-Path -Parent $MyInvocation.MyCommand.Path) 'PSScriptAnalyzerSettings.psd1'),
-    [Switch] $ForGitHubActions
+    [Switch] $ForGitHubActions,
+    [Switch] $ForMsBuild
 )
 
-function Write-FileError([string] $Message, [string] $Path, [int] $Line = 0)
+function Write-FileError([string] $Message, [string] $Path, [int] $Line = 0, [int] $Column = 0)
 {
     if ($Path) { $Path = Get-ChildItem $Path }
 
     if ($ForGitHubActions)
     {
         $Message = $Message -replace '\s*(\r?\n\s*)+', ' '
-        Write-Output $(if ($Path) { "::error file=$Path,line=$Line::$Message" } else { "::error::$Message" })
+        Write-Output $(if ($Path) { "::error file=$Path,line=$Line,col=$Column::$Message" } else { "::error::$Message" })
+    }
+    elseif ($ForMsBuild)
+    {
+        if (-not $Message.Contains(":")) { $Message = ": $Message" }
+
+        echo "IS FILE:  $(-not -not $Path)"
+
+        if ($Path)
+        {
+            Write-Output ("$Path($Line,$Column): error $Message")
+        }
+        else
+        {
+            Write-Output ": error $Message"
+        }
     }
     else
     {
-        Write-Error $(if ($Path) { "[$Path|ln $Line] $Message" } else { $Message })
+        Write-Error $(if ($Path) { "[$Path|ln ${Line}:${Column}] $Message" } else { $Message })
     }
 }
 
@@ -38,7 +54,7 @@ $results = Get-ChildItem -Recurse -Force -Include *.ps1 |
 foreach ($result in $results)
 {
     $message = $result.RuleName + ": " + $result.Message
-    Write-FileError -Path $result.ScriptPath -Line $result.Line $message
+    Write-FileError -Path $result.ScriptPath -Line $result.Line -Column $result.Column $message
 }
 
 exit $results.Count
