31e9ebf03e8d7c6579dfe15182e82e252f9dc754
Dávid El-Saig <david.el-saig@lombiq.com>
2022-07-05 18:46:01 +0200

    Add more tests.

--- Lombiq.Analyzers.PowerShell.Tests/UnitTests/PowerShellAnalysisTests.cs
+++ Lombiq.Analyzers.PowerShell.Tests/UnitTests/PowerShellAnalysisTests.cs
@@ -1,6 +1,8 @@
 ﻿using CliWrap;
 using CliWrap.Buffered;
+using Lombiq.HelpfulLibraries.Cli.Helpers;
 using Shouldly;
+using System;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -26,28 +28,23 @@ public class PowerShellAnalysisTests
             .ExecuteBufferedAsync();
 
         result.ExitCode.ShouldNotBe(0);
-        result.StandardError.ShouldContain("PSAvoidUsingEmptyCatchBlock");
-        result.StandardError.ShouldContain("PSAvoidUsingCmdletAliases");
-        result.StandardError.ShouldContain("PSUseApprovedVerbs");
-        result.StandardError.ShouldContain("PSUseSingularNouns");
+        MessageShouldContainViolationCodes(result.StandardError);
     }
 
     [Theory]
-    public async Task BuildWithPackageReferenceShouldDisplayWarnings(string directory)
+    [InlineData("Lombiq.Analyzers.PowerShell.PackageReference")]
+    [InlineData("Lombiq.Analyzers.PowerShell.ProjectReference")]
+    public async Task BuildShouldDisplayWarnings(string directory)
     {
         if (!await IsPowerShellCoreInstalledAsync()) return;
 
-        var result = await _powerShell
-            .WithWorkingDirectory(Path.GetFullPath(_testSolutions.GetDirectories(directory).Single().FullName))
-            .WithArguments(new[] { "-c", "../Lombiq.Analyzers.PowerShell/Invoke-Analyzer.ps1 -IncludeTestSolutions" })
-            .WithValidation(CommandResultValidation.None)
-            .ExecuteBufferedAsync();
+        var solutionPath = _testSolutions.GetDirectories(directory).Single().FullName;
 
-        result.ExitCode.ShouldNotBe(0);
-        result.StandardError.ShouldContain("PSAvoidUsingEmptyCatchBlock");
-        result.StandardError.ShouldContain("PSAvoidUsingCmdletAliases");
-        result.StandardError.ShouldContain("PSUseApprovedVerbs");
-        result.StandardError.ShouldContain("PSUseSingularNouns");
+        var exception = (InvalidOperationException)await Should.ThrowAsync(
+            () => DotnetBuildHelper.ExecuteStaticCodeAnalysisAsync(solutionPath),
+            typeof(InvalidOperationException));
+
+        MessageShouldContainViolationCodes(exception.Message);
     }
 
     private static async Task<bool> IsPowerShellCoreInstalledAsync()
@@ -61,5 +58,13 @@ public class PowerShellAnalysisTests
         {
             return false;
         }
+    }
+
+    private static void MessageShouldContainViolationCodes(string message)
+    {
+        message.ShouldContain("PSAvoidUsingEmptyCatchBlock");
+        message.ShouldContain("PSAvoidUsingCmdletAliases");
+        message.ShouldContain("PSUseApprovedVerbs");
+        message.ShouldContain("PSUseSingularNouns");
     }
 }
