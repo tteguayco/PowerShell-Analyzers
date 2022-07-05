ea9e2295ff6759f4964e10cb248a50394bac61e1
Dávid El-Saig <david.el-saig@lombiq.com>
2022-07-05 10:55:41 +0200

    Warning no longer needed.

--- Invoke-Analyzer.ps1
+++ Invoke-Analyzer.ps1
@@ -15,7 +15,7 @@ function Write-FileError([string] $Message, [string] $Path, [int] $Line = 0, [in
     if ($ForGitHubActions)
     {
         $Message = $Message -replace '\s*(\r?\n\s*)+', ' '
-        Write-Output $(if ($Path) { "::error file=$Path,line=$Line,col=$Column::$Message" } else { "::error::$Message" })
+        Write-Output "::error" + $(if ($Path) { " file=$Path,line=$Line,col=$Column::$Message" } else { "::$Message" })
     }
     elseif ($ForMsBuild)
     {
