2de89bdd7148eb3711ff903a470e903c226b3380
Benedek Farkas <benedek.farkas@lombiq.com>
2023-01-17 15:39:47 +0100

    Updating Violate-Analyzers.ps1 to violate PSAvoidUsingDoubleQuotesForConstantString only once

--- TestSolutions/Violate-Analyzers.ps1
+++ TestSolutions/Violate-Analyzers.ps1
@@ -1,9 +1,9 @@
 function Violate-Analyzers()
 {
-    Write-Host "This file is intended to verify that PSScriptAnalyzer works and contains intentionally bad code."
+    Write-Host 'This file is intended to verify that PSScriptAnalyzer works and contains intentionally bad code.'
 }
 
 try { Violate-Analyzers } catch { }
 
 "Lombiq", `
-"Orchard", "Hastlayer" | % { $_ }
+'Orchard', 'Hastlayer' | % { $_ }
